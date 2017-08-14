dd070b474a7414b67785a08fe3ada86906a8e1a8
GFX <gustavoferri89@gmail.com>
2017-08-13 21:09:49 -0300

    Primeiro arquivo

---
+++ aula0103a/aula0103a/Computador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula0103a
+{
+    class Computador
+    {
+        //Atributos
+        public String marca;
+        public String cor;
+        public String modelo;
+        public long numeroSerie;
+        private double preco;
+
+        //Métodos
+        public String imprimir()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Computador Marca: ");
+            sb.AppendLine(marca);
+            sb.AppendLine("Modelo: ");
+            sb.AppendLine(modelo);
+            sb.AppendLine("Número de Série: ");
+            sb.AppendLine(numeroSerie.ToString());
+            sb.AppendLine("Preço Atual: ");
+            sb.AppendLine(preco.ToString());
+            return sb.ToString();
+        }
+        public void calcularValor()
+        {
+            if (marca.Equals("HP"))
+            {
+                preco *= 1.30;
+                return;
+            }
+            if (marca.Equals("IBM"))
+            {
+                preco *= 1.40;
+                return;
+            }
+        }
+        public int alterarValor(double valor)
+        {
+            if (valor <= 0)
+            {
+                return 2;
+            }
+            preco = valor;
+            return 1;
+        }
+
+    }
+}
---
+++ aula0103a/aula0103a/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula0103a
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Computador x = new Computador();
+            x.modelo = "Lenovo";
+            x.marca = "IBM";
+            x.cor = "Prata";
+            x.numeroSerie = 123;
+            Console.WriteLine("Digite o valor do computador " + x.modelo);
+            int resposta = x.alterarValor(double.Parse(Console.ReadLine()));
+            if (resposta == 1)
+            {
+                Console.WriteLine("Preço definido com sucesso...");
+            }
+            else
+            {
+                Console.WriteLine("Preço não definido...");
+            }
+            x.calcularValor();
+            Console.WriteLine(x.imprimir());
+            Console.ReadLine();
+
+        }
+    }
+}
